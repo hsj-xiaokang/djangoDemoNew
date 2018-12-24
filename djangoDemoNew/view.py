bf28904380a85146bfdd2aebf5dc7fe5128ee9c3
hsj-xiaokang <2356899074@qq.com>
2018-12-24 13:21:17 +0800

    获取请求头的内容

--- djangoDemoNew/view.py
+++ djangoDemoNew/view.py
@@ -6,12 +6,15 @@
 @software: pycharm
 @file: view.py
 @time: 2018/12/24 12:39
-@desc:view.py
+@desc:view.py -获取请求头的内容
 '''
 from django.http import HttpResponse
 from django.http import HttpRequest
 
 
-def hello(HttpRequest):
-    print HttpRequest
+def hello(request):
+    # for m in request.META:
+    #      print m
+    print request.META.get("HTTP_REFERER")
+    print request.META.get("HTTP_ACCEPT_LANGUAGE")
     return HttpResponse(ur"Hello world-heshengjin-何胜金")
